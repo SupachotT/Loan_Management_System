0ca7b993f1007e6a6bf11d75433427858dc759d9
Supachot <supachot.4762@gmail.com>
2024-07-17 01:08:02 +0700

    [Updated] create function GetApplicantByID, CreateApplicants

--- api/Loan_Applicants/Loan_Applicants.go
+++ api/Loan_Applicants/Loan_Applicants.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
-	_ "github.com/lib/pq" // Import the PostgreSQL driver
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
 )
 
 type Loan_applicants struct {
@@ -137,4 +139,97 @@ func GetApplicants(w http.ResponseWriter, r *http.Request) {
 	// Return JSON response
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(loanApplicants)
+}
+
+func GetApplicantByID(w http.ResponseWriter, r *http.Request) {
+	// Connect to the database
+	connStr := "postgres://Admin:Password@localhost:5432/LMS_LoanApplicantsDB?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Get customer_id from URL parameters
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid applicant ID", http.StatusBadRequest)
+		return
+	}
+
+	// Query database for customer with given applicant_id
+	var loanApplicant Loan_applicants
+	query := `SELECT applicant_id, first_name, last_name, address, phone, email, applicant_status, created_at, updated_at FROM loan_applicants WHERE applicant_id = $1`
+	err = db.QueryRow(query, id).Scan(&loanApplicant.Applicant_id, &loanApplicant.First_name, &loanApplicant.Last_name, &loanApplicant.Address, &loanApplicant.Phone, &loanApplicant.Email, &loanApplicant.Applicant_Status, &loanApplicant.Created_at, &loanApplicant.Updated_at)
+	if err == sql.ErrNoRows {
+		// Return JSON error response if no customer with the given ID exists
+		errorResponse := map[string]string{"error": "applicant not found"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(errorResponse)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Return JSON response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(loanApplicant)
+}
+
+func CreateApplicants(w http.ResponseWriter, r *http.Request) {
+	connStr := "postgres://Admin:Password@localhost:5432/LMS_LoanApplicantsDB?sslmode=disable"
+
+	// Connect to PostgreSQL
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Decode JSON request body into a Customer struct
+	var newApplicant Loan_applicants
+	err = json.NewDecoder(r.Body).Decode(&newApplicant)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Insert query
+	query := `INSERT INTO loan_applicants (first_name, last_name, address, phone, email, applicant_status) VALUES ($1, $2, $3, $4, $5, $6) RETURNING applicant_id`
+	var newApplicantID int
+	err = db.QueryRow(query, newApplicant.First_name, newApplicant.Last_name, newApplicant.Address, newApplicant.Phone, newApplicant.Email, newApplicant.Applicant_Status).Scan(&newApplicantID)
+	if err != nil {
+		pgErr, ok := err.(*pq.Error)
+		if ok && pgErr.Code.Name() == "unique_violation" {
+			// If the error is due to duplicate email, return a specific JSON response
+			errorResponse := map[string]string{
+				"error": fmt.Sprintf("Email '%s' already exists", newApplicant.Email),
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusConflict) // HTTP 409 Conflict
+			json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		// For other errors, return a generic internal server error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Prepare success message
+	successMessage := map[string]interface{}{
+		"message":     "Customer created successfully",
+		"customer_id": newApplicantID,
+	}
+
+	// Set Content-Type and return JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // HTTP 201 Created
+	json.NewEncoder(w).Encode(successMessage)
 }
