d417a5eb4739f027bfd5dd24f5d73b177e17e0c5
Supachot <supachot.4762@gmail.com>
2024-07-16 19:42:19 +0700

    [Created] file main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/SupachotT/Loan_Management_System.git/api/Loan_Applicants"
+)
+
+func main() {
+	// Call the function from the imported package
+	Loan_Applicants.SetupDatabase()
+}
