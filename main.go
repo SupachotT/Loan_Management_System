efae4823918507868bc5813a245f42f9987d7d34
Supachot <supachot.4762@gmail.com>
2024-07-17 20:21:46 +0700

    [Updated] add endpoint for GET data from database LMS_LoanSubmitDB

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/SupachotT/Loan_Management_System.git/api/Loan_Applicants"
@@ -14,22 +13,27 @@ func main() {
 	// Call the function from the imported package
 	Loan_Applicants.SetupDatabase()
 	Loan_Submits.SetupDatabase()
+	// Loan_Submits.TestReadSubmitFromFile()
 
 	// Start server
-	router := setupRouter()
-	fmt.Println("Server listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func setupRouter() *mux.Router {
 	router := mux.NewRouter()
+	handleRoutes(router)
 
-	// Define API endpoints
-	router.HandleFunc("/loan_applicants/all", Loan_Applicants.GetApplicants).Methods("GET")
-	router.HandleFunc("/loan_applicants/{id}", Loan_Applicants.GetApplicantByID).Methods("GET")
-	router.HandleFunc("/loan_applicants/create", Loan_Applicants.CreateApplicants).Methods("POST")
-	router.HandleFunc("/loan_applicants/update/{id}", Loan_Applicants.UpdateApplicants).Methods("PUT")
-	router.HandleFunc("/loan_applicants/delete/{id}", Loan_Applicants.DeleteApplicants).Methods("DELETE")
-
-	return router
+	fmt.Println("Server listening on port 8080...")
+	fmt.Println(http.ListenAndServe(":8080", router))
 }
+
+func handleRoutes(router *mux.Router) {
+	// Define API endpoints for Loan Applicants
+	applicantsRouter := router.PathPrefix("/loan_applicants").Subrouter()
+	applicantsRouter.HandleFunc("/all", Loan_Applicants.GetApplicants).Methods("GET")
+	applicantsRouter.HandleFunc("/{id}", Loan_Applicants.GetApplicantByID).Methods("GET")
+	applicantsRouter.HandleFunc("/create", Loan_Applicants.CreateApplicants).Methods("POST")
+	applicantsRouter.HandleFunc("/update/{id}", Loan_Applicants.UpdateApplicants).Methods("PUT")
+	applicantsRouter.HandleFunc("/delete/{id}", Loan_Applicants.DeleteApplicants).Methods("DELETE")
+
+	// Define API endpoints for Loan Submits
+	submitsRouter := router.PathPrefix("/loan_submits").Subrouter()
+	submitsRouter.HandleFunc("/all", Loan_Submits.GetLoanSubmit).Methods("GET")
+}
+
