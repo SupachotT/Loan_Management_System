8797e6a344ff7abde034014a447ee2a1ab792fc6
Supachot <supachot.4762@gmail.com>
2024-07-17 17:11:33 +0700

    [Updated] seperate endpoint to new function

--- main.go
+++ main.go
@@ -6,13 +6,22 @@ import (
 	"net/http"
 
 	"github.com/SupachotT/Loan_Management_System.git/api/Loan_Applicants"
+	"github.com/SupachotT/Loan_Management_System.git/api/Loan_Submits"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	// Call the function from the imported package
 	Loan_Applicants.SetupDatabase()
+	Loan_Submits.SetupDatabase()
 
+	// Start server
+	router := setupRouter()
+	fmt.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func setupRouter() *mux.Router {
 	router := mux.NewRouter()
 
 	// Define API endpoints
@@ -22,7 +31,5 @@ func main() {
 	router.HandleFunc("/loan_applicants/update/{id}", Loan_Applicants.UpdateApplicants).Methods("PUT")
 	router.HandleFunc("/loan_applicants/delete/{id}", Loan_Applicants.DeleteApplicants).Methods("DELETE")
 
-	// Start server
-	fmt.Println("Server listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	return router
 }
